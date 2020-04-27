a98f8f122ef738bf002f5993c712f9d3e998754e
Moriah Humphries <moriah.humphries@gmail.com>
2020-04-27 16:04:37 -0500

    completed sleep calculator project

--- sleep-calculator.js
+++ sleep-calculator.js
@@ -2,18 +2,51 @@ const getSleepHours = day => {
     if (day === 'monday') {
         return 8;
     } else if (day === 'tuesday') {
-        return 8;
+        return 7;
     } else if (day === 'wednesday') {
-        return 8;
+        return 6;
     } else if (day === 'thursday') {
-        return 8;
+        return 9;
     } else if (day === 'friday') {
-        return 8;
+        return 5;
     } else if (day === 'saturday') {
-        return 8;
+        return 10;
     } else if (day === 'sunday') {
-        return 8;
+        return 11;
     }
+}
+
+
+const getActualSleepHours = () =>
+    getSleepHours('monday') +
+    getSleepHours('tuesday') +
+    getSleepHours('wednesday') +
+    getSleepHours('thursday') +
+    getSleepHours('friday') +
+    getSleepHours('saturday') +
+    getSleepHours('sunday')
+
+
+const getIdealSleepHours = () => {
+    let idealHours = 10;
+    return idealHours * 7;
 };
 
-console.log(getSleepHours('wednesday'));
+const calculateSleepDebt = () => {
+    let actualSleepHours = getActualSleepHours();
+    let idealSleepHours = getIdealSleepHours();
+    if (actualSleepHours === idealSleepHours) {
+        console.log('You got the perfect amount of sleep!')
+    } else if (actualSleepHours > idealSleepHours) {
+        console.log('You got ' + (actualSleepHours = idealSleepHours) + ' hours more than you needed!');
+    } else if (actualSleepHours < idealSleepHours) {
+        console.log('You are missing ' + (idealSleepHours - actualSleepHours) + ' hours of sleep!');
+    }
+
+    console.log(idealSleepHours);
+    console.log(actualSleepHours);
+}
+calculateSleepDebt();
+
+
+
