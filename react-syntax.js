437428c528668077d273f3b456d7ac1546d66350
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-02 14:41:16 -0500

    react component practice with multiline elements

--- react-syntax.js
+++ react-syntax.js
@@ -61,3 +61,21 @@ ReactDOM.render(
     <MyComponentClass />,
     document.getElementById('app')
 );
+
+class QuoteMaker extends React.Component {
+    render() {
+        return (
+            <blockquote>
+                <p>
+                    What is important now is to recover our senses.
+                </p>
+                <cite>
+                    <a target="_blank"
+                       href="https://en.wikipedia.org/wiki/Susan_Sontag">
+                        Susan Sontag
+                    </a>
+                </cite>
+            </blockquote>
+        );
+    }
+}
